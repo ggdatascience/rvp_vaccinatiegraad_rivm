d1adcdce3333cc2a70b685348c98bbcff062e807
Pieter Seinen <44730789+pieterseinen@users.noreply.github.com>
2024-04-16 16:20:41 +0200

    Add files via upload

---
+++ lees_vaccinatiegraad_rivm.R
@@ -0,0 +1,252 @@
+#Lees Vaccinatiegraad RIVM
+#NB We kregen de data aangeleverd met in sheet 2023 geen in CELL A1 
+#(itt de sheets van andere jaren waar TOTAAL NEDERLAND  staat) Deze inconsistentie 
+#zorgt ervoor dat de rij die initieel voor kolomkoppen wordt gebruikt eentje opschuift
+#t.a.v de andere sheets. Dat maakt het onmogelijk om alles tegelijk in te lezen. 
+#Opgelost door zelf iets in A1 van sheet 2023 te zetten.
+
+#workbook laden
+wb <- openxlsx::loadWorkbook(bestand)
+#checken welke sheets er zijn
+sheets <- wb %>% openxlsx::sheets()
+
+#Aanname: Alle met een 4 cijfers is een jaar
+jaren <- sheets[str_detect(sheets,"[:digit:]{4}")]
+
+#DATA NL ophalen
+#Voor iedere sheet met een jaartal: Data NL ophalen. 
+cijfers_nl <- lapply(jaren, function(x){
+  
+  
+  df <- openxlsx::read.xlsx(bestand,sheet = x) %>%
+    #kolommen X47,X48 en X49 bestaan alleen in 2023. 
+    #MenC/ACWY Adolescenten. Verwijderen. 
+    select(-any_of(c("X47","X48","X49")))
+  
+  
+  #Tabellen zijn onhandig geformatteerd voor verwerking in R. 
+  #Informatie die over de inhoud kolommen gaat staat op meerdere rijen en niet boven elke kolom
+  
+  #Vector met bruikbare kolomnamen maken.
+  #Kolominformatie ophalen uit rijen 1:6 (3,8 in excel)
+  kolominfo <- df[1:6,-1] %>%
+    #transponeren zodat we tidyr::fill() kunnen gebruiken
+    t() %>%
+    as.data.frame() %>% 
+    #van boven naar beneden aanvullen waar NA (dus eigenlijk vlnr)
+    fill(everything()) %>%
+    #kolomnaam opbouwen uit alle rij-info excl geboortejaar
+    mutate(kolomnamen = paste(`1`,`2`,`3`,`4`,`6`) %>%
+             str_trim() %>%
+             str_replace_all(" ","_")) %>%
+    pull(kolomnamen) 
+  
+  #kolomnamen globaal toewijzen voor hergebruik in vaccinatiegraad_rivm_ggd
+  kolomnamen <<- c("id",kolominfo)
+  
+  #namen aan df toewijzen
+  names(df) <- kolomnamen
+  
+  df %>%
+    select(#Alleen de variabelen ophalen die gekozen zijn 
+      matches(cohorten) & 
+        matches(vaccinsoort) &
+        matches(vaccinatietoestand)) %>%
+    #Rij 7 = de rij waar de cijfers NL staan (rij 9 in Excel)
+    filter(row_number() %in% c(7)) %>% 
+    #Alles lang maken
+    pivot_longer(everything()) %>%
+    mutate(
+      #Type cijfer ophalen (aantal gevaccineerd,aantal clienten en %)
+      type_cijfer = str_extract(name,"(?<=jaar_).*"),
+      #Inhoudelijke naam ophalen
+      name = str_remove(name,"(?<=jaar).*")) %>%
+    #Weer breed maken; kolom per type cijfer
+    pivot_wider(values_from = value,
+                names_from = type_cijfer) %>%
+    #Jaar; aanpassen v. rapportage jaar naar werkelijk jaar
+    mutate(jaar = as.numeric(x) - 1)
+  
+}) %>% 
+  #dfs per jaar samenvoegen
+  do.call(rbind,.) %>%
+  mutate(`%` = as.numeric(`%`)*100 %>% round(1),
+         #Label v. NL cijfers maken: naam vaccin + cohortnaam + leeftijd/vaccinstatus
+         name =  paste(
+           #Naam vaccin
+           str_extract(name, glue("(?<={cohorten}_)[:alpha:]*")),
+           #Cohortnaam
+           str_extract(name, glue("{cohorten}")) %>% str_remove("_"),
+           #linebreak
+           "\n",
+           #Leeftijd off vaccinatietoestand
+           case_when(vacctoestand_ipv_leeftijd ~ str_extract(name,vaccinatietoestand) %>%
+                       str_replace_all("_"," "),
+                     TRUE ~ paste(str_extract(name, "[:digit:]+"), "jaar")
+                     ))
+          )
+
+
+#Voor iedere sheet met een jaartal: data per pc4 & gemeente ophalen
+vaccinatiegraad_rivm_ggd <- lapply(jaren, function(x){
+  
+  df <- openxlsx::read.xlsx(bestand,sheet = x) %>%
+    #kolommen X47,X48 en X49 bestaan alleen in 2023. 
+    #MenC/ACWY Adolescenten. Verwijderen. 
+    select(-any_of(c("X47","X48","X49")))
+  
+  #namen kolommen toewijzen (gedefinieerd in cijfers_nl)
+  names(df) <- kolomnamen
+  
+  #Tabel bij begint en eind afknippen .
+  start_tabel = which(df$id == "Rijlabels")
+  eind_tabel = which(df$id == "Eindtotaal")
+  
+  df <- df %>%
+    select(id,
+           #Alleen ophalen wat boven is opgegeven
+           matches(cohorten) &
+           matches(vaccinsoort) &
+           matches(vaccinatietoestand)) %>%
+    #tabel filteren op rijnummers waar gemeente & pc4 data in zit 
+    filter(row_number() > start_tabel & row_number() < eind_tabel) %>%
+    #Gemeentenaam uit rij met postcodes halen voor apparte kolom
+    mutate(gemeentenaam = case_when(
+      #Als er een letter in de variabele zit is het een gemeente ipv postcode
+      str_detect(id, "[:alpha:]") ~ id,
+      TRUE ~ NA)) %>%
+    #gemeentenaam aanvullen van boven naar beneden
+    tidyr::fill(gemeentenaam, .direction = "down") 
+  
+  df %>%
+    #Jaar; aanpassen v. rapportage jaar naar werkelijk jaar
+    mutate(jaar = as.numeric(x) -1) %>%
+    #alle aantallen naar numeric & percentage afronden
+    mutate(across(contains("_"), as.numeric),
+           across(contains("%"),~ round(.x * 100, 1))) %>%
+    #Na's vervangen met 0 zodat NA als <5 clienten geteld kan worden
+    mutate_if(is.numeric, replace_na,0) 
+}) %>% 
+  #dataframes koppelen
+  do.call(rbind,.) 
+
+#Op verzoek:  Kaart zonder exacte percentages maken.maar met categorieën
+if(!percentages_in_kaart) {
+  
+  vaccinatiegraad_rivm_ggd <- vaccinatiegraad_rivm_ggd %>%
+    mutate(across(contains("%"), ~
+                    
+                    case_when(.x < 90 ~ "< 90%",
+                              .x < 95 ~ "90-94%",
+                              .x >= 95 ~ ">= 95%",
+                              TRUE ~ "?"
+                    )))
+}
+
+#Sheetnamen in RIVM excel zijn rapportagejaren. gaat over voorgaande jaren. dus: -1
+jaren <- as.numeric(jaren) - 1
+
+#Gemeentedata uit df halen; als er tekst in het 'id' veld staat is het een gemeente
+vaccinatiegraad_gemeente <- vaccinatiegraad_rivm_ggd %>%
+  filter(id %>% str_detect("[:alpha:]"))
+
+gemeente_sf <- st_read("shapefiles/cbsgebiedsindelingen2022.gpkg",
+                       layer = "gemeente_gegeneraliseerd",
+                       quiet = T) %>%
+  select(-id) %>%
+  left_join(vaccinatiegraad_gemeente, by = c("statnaam" = "gemeentenaam")) %>%
+  #Waar "jaar" missing is = is het geen HvB gemeente
+  filter(!is.na(jaar)) %>%
+  mutate(niveau = "gemeente",
+         gemeentenaam = id) %>%
+  st_transform(4326)
+
+
+#Postcodedata uit df halen
+vaccinatiegraad_pc4 <- vaccinatiegraad_rivm_ggd %>% 
+  filter(id %>% str_detect("[:digit:]"))
+
+#PC4 is niet alle jaren gelijk; missing pc4 als lege "grijze" polygonen weergeven in die jaren. 
+#PC4 aanvullen in jaren waar er pc4 ontbreken
+
+#koppeltabel maken van gemeentenamen en pc4
+gemeentenamen_pc4 <- vaccinatiegraad_pc4 %>%
+  group_by(id, gemeentenaam) %>%
+  summarise() %>%
+  ungroup()
+
+
+#dataframe met alle mogelijke combinaties pc4/jaar maken
+pc4_jaar_df <- expand.grid(c(gemeentenamen_pc4$id,
+                             #Eventueel handmatig toegevoegde postcodes meenemen
+                             toegevoegde_postcodes$PC4),jaren) %>%
+  #gemeentenaam koppelen
+  left_join(gemeentenamen_pc4, by = c("Var1" = "id"))
+
+#Als er handmatig postcodes zijn toegevoegd; pas dan ook de gemeentenaam aan.
+if(nrow(toegevoegde_postcodes) > 0){
+  pc4_jaar_df <- pc4_jaar_df %>%
+    left_join(toegevoegde_postcodes, by = c("Var1" = "PC4")) %>%
+    #PC4 5048 nog een gemeentenaam geven
+    mutate(gemeentenaam = ifelse(is.na(gemeentenaam.x), gemeentenaam.y,
+                                 gemeentenaam.x)) %>%
+    select(-c(gemeentenaam.x,gemeentenaam.y))
+    
+}
+
+
+
+#kolomnamen gelijk trekken met vaccinatiegraad_pc4
+names(pc4_jaar_df) <- c("id","jaar", "gemeentenaam_2")
+
+#Lege pc4 aan vaccinatiegraaddata koppelen
+vaccinatiegraad_pc4 <- vaccinatiegraad_pc4 %>%
+  full_join(pc4_jaar_df, by = c("id","jaar")) %>%
+  mutate(gemeentenaam = ifelse(is.na(gemeentenaam),
+                               gemeentenaam_2,
+                               gemeentenaam)) %>%
+  select(-gemeentenaam_2) %>%
+  mutate_if(is.numeric, replace_na,0)
+
+
+#PC4 shapefile inlezen en vaccinatiedata data koppelen  
+pc4_sf <- st_read("shapefiles/PC4.shp",
+                  quiet = T) %>%
+  left_join(vaccinatiegraad_pc4, by = c("PC4" = "id")) %>%
+  mutate(id = PC4,
+         niveau = "PC4") %>%
+  #Alle PC4 waar jaar missing is, zijn niet in HvB
+  filter(!is.na(jaar)) %>%
+  st_transform(4326)
+
+
+
+#Dataframe met labels voor kaartlagen maken
+categorieen <- names(vaccinatiegraad_rivm_ggd %>% select(-c(id,gemeentenaam,jaar))) %>%
+  str_remove("_Aantal.*|_%") %>%
+  unique()
+
+niveaus <- c("PC4","gemeente")
+
+#Alle combinaties van categorie & nivea, + een jaarvariabele
+kaartlagen <- expand.grid(categorieen, max(jaren), niveaus) %>%
+  data.frame()
+
+names(kaartlagen) <- c("categorie","jaar","niveau") 
+
+kaartlagen <- kaartlagen %>% 
+  mutate(
+    categorie = as.character(categorie), #van factor naar character
+    cohort = str_extract(categorie,cohorten),
+    vaccinsoort = str_extract(categorie,glue("(?<={cohort}_)[:alpha:]*")),
+    #o.b.v. de variabele vacctoestand_ipv_leeftijd
+    #Wordt een leeftijd of de vaccinatietoestand uit de categorie gehaald
+    leeftijd_of_vaccinatietoestand =  case_when(
+      vacctoestand_ipv_leeftijd ~ str_extract(categorie, vaccinatietoestand) %>%
+        str_replace_all("_"," "),
+      TRUE ~ str_extract(categorie,"[:digit:]+") %>% paste("jaar")),
+    naam_noemer = glue("{cohort} ({leeftijd_of_vaccinatietoestand})"),
+    cohort = str_remove(cohort,"_"),    #_ in 'adolescente_meisjes' verwijderen
+    groepnaam = glue("{vaccinsoort} {cohort} ({leeftijd_of_vaccinatietoestand}) per {niveau} {max(jaren)}"),
+    data = case_when(niveau == "PC4" ~ "pc4_sf",
+                     niveau == "gemeente" ~ "gemeente_sf"))
---
+++ maak_polygon_layer_vaccinatiegraad.R
@@ -0,0 +1,174 @@
+#Functie om polygoonlaag te maken voor leaflet kaart RIVM vaccinatiegraden op PC4 en gemeenteniveau
+maak_leaflet_polygon_layer <- function(kaart, data, huidig_jaar, categorie,groepnaam, 
+                                       naam_noemer, percentages_in_kaart = T, 
+                                       kleuren_graad = c("green" = 95,
+                                                         "#FB6A4A" = 90,
+                                                         "#EF3B2C" = 85,
+                                                         "#A50F15" = 75,
+                                                         "#67000D" = 0)
+                                       ){
+  
+# print(glue("{groepnaam} verwerken"))
+
+#referenties naar relevante variabelen maken
+clienten = glue("{categorie}_Aantal_clienten")
+percentage = glue("{categorie}_%")
+
+#kleuren vacc. graad: waarden & kleurcodes uit elkaar halen
+afkapwaarden <- kleuren_graad %>% unname()
+kleuren <- kleuren_graad %>% names()
+
+
+if(percentages_in_kaart){
+#Kleuren aan dataframe toewijzen o.b.v vaccinatiegraad
+data = data %>%
+  mutate(
+    
+    kleuren = case_when(
+      #Minder dan 5 clienten = niet laten zien
+      !!sym(clienten) < minimum_verbergen ~ "grey",
+      #95% of hoger = WHO norm
+      !!sym(percentage) >= afkapwaarden[1] ~ kleuren[1],
+      #Alles daaronder is verschillende graden van slecht
+      !!sym(percentage) >= afkapwaarden[2] ~ kleuren[2],
+      !!sym(percentage) >= afkapwaarden[3] ~  kleuren[3],
+      !!sym(percentage) >= afkapwaarden[4] ~  kleuren[4],
+      TRUE ~  kleuren[5])
+    )
+
+} else{
+
+  #Kleuren anders toewijzen wanneer er voor !percentages_in_kaart is gekozen
+  data = data %>%
+    mutate(kleuren = case_when(
+      !!sym(clienten) < minimum_verbergen ~ "grey",
+      !!sym(percentage) == ">= 95%" ~  "#004529",
+      !!sym(percentage) == "90-94%" ~  "#ADDD8E",
+      !!sym(percentage) == "< 90%" ~  "#A50F15",
+      
+    ))
+}
+
+#Voor de polygoon willen we alleen het laatste jaar laten zien
+sf <- data %>% filter(jaar == huidig_jaar)
+
+#Popup Labels maken per polygoon
+labels <- lapply(sf$id, function(x){
+  
+  #Subset maken voor specifieke gemeente/pc4
+  temp_df = sf %>% filter(id == x)
+  
+  #Naam gebied toewijzen
+  naam = case_when(
+    #Label gebied voor "gemeente"
+    temp_df$niveau == "gemeente" ~ glue("<h6>{temp_df$id}</h6>"),
+    #Label gebied voor PC4
+    temp_df$niveau == "PC4" ~ glue("<h6> PC4: {temp_df$id} ({temp_df$gemeentenaam}) </h6>"),
+    )
+    
+
+  #Als < 5 clienten niks weergeven & functie afbreken
+  if(temp_df[[clienten]] < minimum_verbergen){
+    
+    return(glue("<h5>Vaccinatiegraad {groepnaam}</h5>
+                {naam}
+                <p>Aantal {naam_noemer} te laag om vaccinatiegraad te weergeven (< {minimum_verbergen})</p>"))
+    
+  }
+  
+  #lege disclaimervar maken, vullen als clienten < minimum_waarschuwing
+  disclaimer = ""
+  if(temp_df[[clienten]] < minimum_waarschuwing){
+
+    disclaimer = glue("<p style='color:red;' > LET OP: Minder dan {minimum_waarschuwing} {naam_noemer} </p>")
+  }
+  
+  #Bij percentages boven de 95% willen we niet het exacte percentage laten zien maar ">=95%"
+  if(temp_df[[percentage]] >= 95 | temp_df[[percentage]] == ">= 95%" ){
+    melding = glue("<strong><span style='color:{temp_df$kleuren}'> >95% </span></strong> uit totaal {temp_df[[clienten]]} {naam_noemer}")
+  } else{
+    melding = glue("<strong><span style='color:{temp_df$kleuren}'>{temp_df[[percentage]]}% </span></strong> (uit totaal {temp_df[[clienten]]} {naam_noemer})")
+  }
+  
+  #Data uit meerjaren_df halen voor trendlijntje & kleurtjes
+  trend_df <- data %>% filter(id == x)
+  
+  #Trendwaarden als vector opslaan; alles op of hoger dan 95 naar 95.
+  if(percentages_in_kaart){
+    trendwaarden <- ifelse(trend_df[[percentage]] >= 95, 95,trend_df[[percentage]])
+    
+  } else{
+  
+    #als we categorieen willen laten zien kunnen we niet een trendlijn
+    #maken op de exacte cijfers. Die zijn er immers niet.
+    #Daarom 1,2,3. zodat we  kunnen laten zien wat hoger / lager is
+    trendwaarden <- case_when(trend_df[[percentage]] == "< 90%" ~ 1,
+                              trend_df[[percentage]] == "90-94%" ~ 2,
+                              trend_df[[percentage]] == ">= 95%" ~ 3,
+                              
+                              )
+    
+  }
+
+  trend <- trend_df %>%
+    select(jaar,!!sym(percentage), kleuren) %>%
+    #>= 95 niet exact laten zien
+    mutate(percentage = case_when(
+      !!sym(percentage) >= 95 ~ ">= 95",
+      TRUE ~ as.character(!!sym(percentage))
+      ),
+      #Kleurcodering cellen:
+      percentage = cell_spec(percentage,
+                             background = kleuren,
+                             color = "white")) %>%
+    data.frame() %>%
+    select(jaar,percentage) %>%
+    #Breed maken
+    pivot_wider(names_from = jaar, values_from = percentage) %>%
+    #lege trendlijnvariabele maken
+    mutate(trendlijn = "") %>%
+    kableExtra::kable(escape = F,
+                      format = "html") %>%
+    kable_minimal() %>%
+    #Plaatje van de trendlijn toevoegen
+    column_spec(4, image = spec_plot(list(trendwaarden), same_lim = T))
+    
+  #Werkelij de popup samenstellen
+  return(
+    glue(
+      "
+      <h5>Vaccinatiegraad {groepnaam}</h5>
+      {naam}
+      {disclaimer}
+      {melding}
+      {trend}
+      "
+      
+    )
+    
+  )
+
+  
+})
+
+
+  #Kaartlaag toevoegen
+  kaart %>%
+  addPolygons(data        = sf,
+              color       = "black", 
+              fillColor   = sf$kleuren,
+              opacity     = 0.5,
+              weight      = 1,
+              fillOpacity = 0.5,
+              group       = groepnaam,
+              popup       = labels,
+              labelOptions = labelOptions(
+                style = list(direction = "auto")),
+              highlightOptions = highlightOptions(color = "black", weight = 3,
+                                                  bringToFront = TRUE))
+  
+
+
+}
+
+
